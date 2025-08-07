1ecfad15d7abc0c268710083da81fb9bb4e71dde
Hackall <36754621+hackall360@users.noreply.github.com>
2025-08-07 00:38:57 -0700

    Implement RSA/AES helpers

--- src/lib/net/crates/encryption/src/errors.rs
+++ src/lib/net/crates/encryption/src/errors.rs
@@ -2,6 +2,12 @@ use thiserror::Error;
 
 #[derive(Debug, Clone, Error)]
 pub enum NetEncryptionError {
-    #[error("Something failed lol")]
-    SomeError,
+    #[error("RSA error: {0}")]
+    RsaError(String),
+
+    #[error("AES error: {0}")]
+    AesError(String),
+
+    #[error("Invalid verify token")]
+    InvalidVerifyToken,
 }

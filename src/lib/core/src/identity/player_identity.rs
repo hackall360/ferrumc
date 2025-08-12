a26c51c9a9db3479f04271b02b93eeeb7fe2eda6
Hackall <36754621+hackall360@users.noreply.github.com>
2025-08-11 20:31:24 -0700

    feat: add permission levels and command feedback

--- src/lib/core/src/identity/player_identity.rs
+++ src/lib/core/src/identity/player_identity.rs
@@ -1,11 +1,21 @@
 use bevy_ecs::prelude::Component;
 use typename::TypeName;
 
+/// Identity information for a player, including permission level.
+///
+/// # Examples
+/// ```
+/// use ferrumc_core::identity::player_identity::PlayerIdentity;
+/// let id = PlayerIdentity::new("Steve".into(), 42);
+/// assert_eq!(id.permission_level, 0);
+/// ```
 #[derive(TypeName, Debug, Component, Default, Clone)]
 pub struct PlayerIdentity {
     pub username: String,
     pub uuid: uuid::Uuid,
     pub short_uuid: i32,
+    /// Permission level of the player.
+    pub permission_level: u8,
 }
 
 impl PlayerIdentity {
@@ -14,6 +24,7 @@ impl PlayerIdentity {
             username,
             uuid: uuid::Uuid::from_u128(uuid),
             short_uuid: uuid as i32,
+            permission_level: 0,
         }
     }
 }
